4dff601d4015b0d128c20e4b9d28ad5fb1f2ee8e
soldelotus <130674717+soldelotus@users.noreply.github.com>
2024-03-26 07:25:24 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,20 @@
 import { Link } from "react-router-dom";
-import Navbar from "../components/Navbar";
  
-const Contact = () => {
+const Navbar = () => {
     return (
-        <div className="contatos">
-            <Navbar />
-            <h1>Página de contato</h1>
-            {/* 5 - nested routes */}
-            <div className="divcontato">
- 
-                <p className="textosol">Me chamo Sol, sou estudante de Sistemas para Internet na Universidade Católica de pernambuco.
-            Sou taróloga, modelo, cineasta, cantora, poetisa, programadora e coisas mais.
-            Procuro um ambiente no qual posso ser eu mesmo e viver tudo que me deixa feliz</p>
- 
-                <div className="links">
- 
-                    <a href="https://www.linkedin.com/in/soldelotus" target="_blank"><button className="botao">
-                        Linkedin
-                        </button></a>
- 
-                    <a href="https://github.com/soldelotus" target="_blank">
-                    <button className="botao">GitHub
-                    </button></a>
-                </div>
-               
-            </div>
-         
-        </div>
+        <nav className="navbar">
+            <Link to="/">
+                <button className="botao">
+                    Home
+                </button>
+            </Link>
+            <Link to="/contact">
+                <button className="botao">
+                    Contato
+                </button>
+            </Link>
+        </nav>
     )
 }
  
-export default Contact
+export default Navbar
